dec5df99de39f0740f6fe24a1475d2e4c22f074c
Frikha Ahmad <59843500+IceBreaker8@users.noreply.github.com>
2021-08-03 16:32:25 +0100

    Update DemoApplicationTests.java

--- src/test/java/com/blog/demo/DemoApplicationTests.java
+++ src/test/java/com/blog/demo/DemoApplicationTests.java
@@ -2,7 +2,7 @@ package com.blog.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-// test
+// testsss
 @SpringBootTest
 class DemoApplicationTests {
 
